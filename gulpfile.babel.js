026bbdaf3e460a9a32ee8494e1ac1d6fca2f98a1
Carl Uebelhart <carl@erutan.net>
2016-02-28 18:53:48 -0300

    meh
    https://github.com/zurb/foundation-sites/issues/8256

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,7 +10,7 @@ import sherpa   from 'style-sherpa';
 import yaml     from 'js-yaml';
 import fs       from 'fs';
 import access   from 'gulp-accessibility';
-// import aria     from 'gulp-arialinter';
+// import arialinter  from 'gulp-arialinter';
 
 // Load all Gulp plugins into one variable
 const $ = plugins();
@@ -28,7 +28,7 @@ function loadConfig() {
 
 // Build the "dist" folder by running all of the below tasks
 gulp.task('build',
- gulp.series(clean, gulp.parallel(pages, sass, javascript, images, copy, copy-bower), styleGuide));
+ gulp.series(clean, gulp.parallel(pages, sass, javascript, images, copy, copyBower), styleGuide));
 
 // Build the site, run the server, and watch for file changes
 gulp.task('default',
@@ -47,8 +47,7 @@ function copy() {
     .pipe(gulp.dest('dist/assets'));
 }
 
-// Copy select files from bower-components to be loaded separately
-function copy-bower() {
+function copyBower() {
   return gulp.src(PATHS.bowerDirectLinked)
     .pipe(gulp.dest('dist/assets/bower_components'));
 });
